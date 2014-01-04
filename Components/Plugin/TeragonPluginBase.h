e5c974fbc285809933ba7ce0328921eb5c63bcd2
Nik Reiman <nik.reiman@gmail.com>
2014-01-04 15:59:40 +0100

    Version 1.4.2

--- Components/Plugin/TeragonPluginBase.h
+++ Components/Plugin/TeragonPluginBase.h
@@ -1,15 +1,26 @@
 /*
-  ==============================================================================
+ * Copyright (c) 2013 - Teragon Audio LLC
+ *
+ * Permission is granted to use this software under the terms of either:
+ * a) the GPL v2 (or any later version)
+ * b) the Affero GPL v3
+ *
+ * Details of these licenses can be found at: www.gnu.org/licenses
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * ------------------------------------------------------------------------------
+ *
+ * This software uses the JUCE library.
+ *
+ * To release a closed-source product which uses JUCE, commercial licenses are
+ * available: visit www.juce.com for more information.
+ */
 
-    This file was auto-generated!
-
-    It contains the basic startup code for a Juce application.
-
-  ==============================================================================
-*/
-
-#ifndef PLUGINPROCESSOR_H_INCLUDED
-#define PLUGINPROCESSOR_H_INCLUDED
+#ifndef TERAGONPLUGINBASE_H_INCLUDED
+#define TERAGONPLUGINBASE_H_INCLUDED
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "PluginParameters.h"
@@ -67,4 +78,5 @@ protected:
 
 } // namespace teragon
 
-#endif  // PLUGINPROCESSOR_H_INCLUDED
+#endif  // TERAGONPLUGINBASE_H_INCLUDED
+
