9809f41e7ca83b0590ba168df9459a0b6a74a69f
Tomas Medek <tom@virtualanalogy.com>
2014-07-26 22:54:46 +0200

    Paraphrasis is only realtime now. Sinewave oscilator removed.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -26,130 +26,6 @@
   #define FUNDAMENTAL_FREQUENCY 109 //pno
 #endif
 
-#define REAL_TIME
-
-//==============================================================================
-/** A demo synth sound that's just a basic sine wave.. */
-class SineWaveSound : public SynthesiserSound
-{
-public:
-    SineWaveSound() {}
-
-    bool appliesToNote(const int /*midiNoteNumber*/)
-    {
-        return true;
-    }
-    bool appliesToChannel(const int /*midiChannel*/)
-    {
-        return true;
-    }
-};
-
-//==============================================================================
-/** A simple demo synth voice that just plays a sine wave.. */
-class SineWaveVoice  : public SynthesiserVoice
-{
-public:
-    SineWaveVoice()
-        : angleDelta(0.0),
-          tailOff(0.0)
-    {
-    }
-
-    bool canPlaySound(SynthesiserSound* sound)
-    {
-        return dynamic_cast <SineWaveSound*>(sound) != 0;
-    }
-
-    void startNote(int midiNoteNumber, float velocity,
-                   SynthesiserSound* /*sound*/, int /*currentPitchWheelPosition*/)
-    {
-        currentAngle = 0.0;
-        level = velocity * 0.15;
-        tailOff = 0.0;
-
-        double cyclesPerSecond = MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-        double cyclesPerSample = cyclesPerSecond / getSampleRate();
-
-        angleDelta = cyclesPerSample * 2.0 * double_Pi;
-    }
-
-    void stopNote(bool allowTailOff)
-    {
-        if (allowTailOff)
-        {
-            // start a tail-off by setting this flag. The render callback will pick up on
-            // this and do a fade out, calling clearCurrentNote() when it's finished.
-
-            if (tailOff == 0.0) // we only need to begin a tail-off if it's not already doing so - the
-                // stopNote method could be called more than once.
-                tailOff = 1.0;
-        }
-        else
-        {
-            // we're being told to stop playing immediately, so reset everything..
-
-            clearCurrentNote();
-            angleDelta = 0.0;
-        }
-    }
-
-    void pitchWheelMoved(int /*newValue*/)
-    {
-        // can't be bothered implementing this for the demo!
-    }
-
-    void controllerMoved(int /*controllerNumber*/, int /*newValue*/)
-    {
-        // not interested in controllers in this case.
-    }
-
-    void renderNextBlock(AudioSampleBuffer& outputBuffer, int startSample, int numSamples)
-    {
-        if (angleDelta != 0.0)
-        {
-            if (tailOff > 0)
-            {
-                while (--numSamples >= 0)
-                {
-                    const float currentSample = (float)(sin(currentAngle) * level * tailOff);
-
-                    for (int i = outputBuffer.getNumChannels(); --i >= 0;)
-                        outputBuffer.addSample(i, startSample, currentSample);
-
-                    currentAngle += angleDelta;
-                    ++startSample;
-
-                    tailOff *= 0.99;
-
-                    if (tailOff <= 0.005)
-                    {
-                        clearCurrentNote();
-
-                        angleDelta = 0.0;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                while (--numSamples >= 0)
-                {
-                    const float currentSample = (float)(sin(currentAngle) * level);
-
-                    for (int i = outputBuffer.getNumChannels(); --i >= 0;)
-                        outputBuffer.addSample(i, startSample, currentSample);
-
-                    currentAngle += angleDelta;
-                    ++startSample;
-                }
-            }
-        }
-    }
-
-private:
-    double currentAngle, angleDelta, level, tailOff;
-};
 //==============================================================================
 class LorisSound : public SynthesiserSound
 {
@@ -183,10 +59,11 @@ public:
             this->partials.push_back(*it);
             it++;
         }
-#ifdef REAL_TIME
+
         synth.setupRealtime(partials);
-#endif
+
         lastFreqMultiplyer = 1.0;
+        play = false;
     }
 
     bool canPlaySound(SynthesiserSound* sound)
@@ -199,23 +76,34 @@ public:
     {
         level = velocity * 0.15;
         sampleIndex = 0;
-
+        tailOff = 0.0;
+        
         double freqMultiplyer = MidiMessage::getMidiNoteInHertz (midiNoteNumber) / FUNDAMENTAL_FREQUENCY;
     
         synth.resetSynth(freqMultiplyer / lastFreqMultiplyer);//scale back
         
         lastFreqMultiplyer = freqMultiplyer;
-    
-        playNote = true;
-
+        play = true;
     }
 
     void stopNote(bool allowTailOff)
     {
-        sampleIndex = 0;
-        clearCurrentNote();
-        playNote = false;
-        first--;
+        if (allowTailOff)
+        {
+            // start a tail-off by setting this flag. The render callback will pick up on
+            // this and do a fade out, calling clearCurrentNote() when it's finished.
+            
+            if (tailOff == 0.0) // we only need to begin a tail-off if it's not already doing so - the
+                // stopNote method could be called more than once.
+                tailOff = 1.0;
+        }
+        else
+        {
+            // we're being told to stop playing immediately, so reset everything..
+            sampleIndex = 0;
+            play = false;
+            clearCurrentNote();
+        }
     }
 
     void pitchWheelMoved(int /*newValue*/)
@@ -230,57 +118,61 @@ public:
 
     void renderNextBlock(AudioSampleBuffer& outputBuffer, int startSample, int numSamples)
     {
-        if (!playNote) return;
+        if ( !play ) return;
+        
+        synth.synthesizeNext(numSamples);
         
         
-    
-#ifndef REAL_TIME
-        const double startTime = sampleIndex * secPerSample;
-        const double endTime = (sampleIndex + numSamples) * secPerSample;
-        
-        filteredPartials.clear();
-        buffer.clear();
-        auto it = partials.begin();
-        while ( it != partials.end() )
+        if (tailOff > 0.)
         {
-            if ( it->endTime() >= startTime && it->startTime() <= endTime )
+            while (--numSamples >= 0)
             {
-                filteredPartials.push_back( *it );
+                double currentSample = sampleIndex < buffer.size() ? buffer[sampleIndex] * tailOff : 0.0;
+                
+                tailOff *= 0.99;
+                
+                if (tailOff <= 0.005)
+                {
+                    tailOff = 0.;
+                    sampleIndex = 0;
+                    clearCurrentNote();
+                    play = false;
+                    break;
+                }
+
+                for (int i = outputBuffer.getNumChannels(); --i >= 0;)
+                    outputBuffer.addSample(i, startSample, currentSample);
+
+                ++startSample;
+                ++sampleIndex;
             }
-            it++;
         }
-        synth.synthesize(filteredPartials.begin(), filteredPartials.end());
-#else
-//        if (first > 0)
-            synth.synthesizeNext(numSamples);
-#endif
-        while (--numSamples >= 0)
+        else
         {
-            double currentSample = sampleIndex < buffer.size() ? buffer[sampleIndex] : 0.0;
-
-            for (int i = outputBuffer.getNumChannels(); --i >= 0;)
-                outputBuffer.addSample(i, startSample, currentSample);
-
-            ++startSample;
-            ++sampleIndex;
+            while (--numSamples >= 0)
+            {
+                double currentSample = sampleIndex < buffer.size() ? buffer[sampleIndex] : 0.0;
+                
+                for (int i = outputBuffer.getNumChannels(); --i >= 0;)
+                    outputBuffer.addSample(i, startSample, currentSample);
+                
+                ++startSample;
+                ++sampleIndex;
+            }
         }
     }
 
 private:
     double level, tailOff;
     int sampleIndex;
-    Loris::PartialList partials;
     std::vector<double> buffer;
     double secPerSample;
+    
+    Loris::PartialList partials;
     Loris::PartialList filteredPartials;
-#ifndef REAL_TIME
-    Loris::Synthesizer synth;
-#else
     Loris::RealTimeSynthesizer synth;
-#endif
     double lastFreqMultiplyer;
-    bool playNote;
-    int first = 2;
+    bool play;
 };
 
 
