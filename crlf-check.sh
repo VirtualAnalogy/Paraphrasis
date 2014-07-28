0aa389ab616ac1126ea54bf3bba1e34701b2f081
Tomas Medek <tom@virtualanalogy.com>
2014-07-28 12:46:18 +0200

    ThirdParty folder added, Loris moved in

---
+++ crlf-check.sh
@@ -0,0 +1,6 @@
+dos2unix Builds/MacOSX/Info.plist
+dos2unix Builds/MacOSX/Paraphrasis.xcodeproj/project.pbxproj
+dos2unix JuceLibraryCode/JuceHeader.h
+dos2unix JuceLibraryCode/AppConfig.h
+dos2unix JuceLibraryCode/ReadMe.txt
+dos2unix Paraphrasis.jucer
