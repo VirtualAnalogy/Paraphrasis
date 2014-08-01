89b9ea98116f0f99f642d2c987b64fcf643b1ca2
Tomas Medek <tom@virtualanalogy.com>
2014-08-01 04:20:56 +0200

    Pitch detection for selected sample added

--- Source/ParameterDefitions.h
+++ Source/ParameterDefitions.h
@@ -21,6 +21,8 @@ static const  int kParameterFrequencyResolution_minValue = 30;
 static const  int kParameterFrequencyResolution_maxValue = 10000;
 static const  int kParameterFrequencyResolution_defaultValue = 40;
 
+static const double kDefaultPitchResolutionRation = 0.8;
+
 static const char* kParameterLastSamplePath_name = "Last Sample Path";
 
 
