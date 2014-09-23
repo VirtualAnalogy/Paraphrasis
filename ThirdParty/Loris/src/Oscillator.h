07f88e4f6b5109e73c4fc17e3192e7cc1b2f0127
Tomas Medek <tom@virtualanalogy.com>
2014-09-23 13:54:49 +0200

    New Loris::RealtimeOscillator class added. It is based on original Loris::Oscillator. Uses SSE2. Loris::Oscillator is reverted to original version with little changes to make inheritance possible.

--- ThirdParty/Loris/src/Oscillator.h
+++ ThirdParty/Loris/src/Oscillator.h
@@ -1,11 +1,11 @@
 #ifndef INCLUDE_OSCILLATOR_H
 #define INCLUDE_OSCILLATOR_H
 /*
- * This is the Loris C++ Class Library, implementing analysis, 
- * manipulation, and synthesis of digitized sounds using the Reassigned 
+ * This is the Loris C++ Class Library, implementing analysis,
+ * manipulation, and synthesis of digitized sounds using the Reassigned
  * Bandwidth-Enhanced Additive Sound Model.
  *
- * Loris is Copyright (c) 1999-2010 by Kelly Fitz and Lippold Haken
+ * Loris is Copyright (c) 1999-2014 by Kelly Fitz and Lippold Haken
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,7 +37,8 @@
 #include "Filter.h"
 
 //  begin namespace
-namespace Loris {
+namespace Loris
+{
 
 class Breakpoint;
 
@@ -48,57 +49,54 @@ class Breakpoint;
 //! sinusoidal oscillator used for synthesizing sounds from Reassigned
 //! Bandwidth-Enhanced analysis data. Oscillator encapsulates the oscillator
 //! state, including the instantaneous radian frequency (radians per
-//! sample), amplitude, bandwidth coefficient, and phase, and a 
-//! bandlimited stochastic modulator. 
+//! sample), amplitude, bandwidth coefficient, and phase, and a
+//! bandlimited stochastic modulator.
 //!
 //! Class Synthesizer uses an instance of Oscillator to synthesize
 //! bandwidth-enhanced Partials.
 //
 class Oscillator
 {
-//  --- implementation ---
+protected:
+    //  --- implementation ---
 
     NoiseGenerator m_modulator;     //! stochastic modulator
     Filter m_filter;                //! filter applied to the noise generator
-    
+
     //  instantaneous oscillator state:
     double m_instfrequency;         //! radians per sample
     double m_instamplitude;         //! absolute amplitude
     double m_instbandwidth;         //! bandwidth coefficient (noise energy / total energy)
-    
+
     //  accumulating phase state:
     double m_determphase;       //! deterministic phase in radians
-    
-    double m_frequencyScaling;
 
-//  --- interface ---
+    //  --- interface ---
 public:
-//  --- construction ---
+    //  --- construction ---
 
     //! Construct a new Oscillator with all state parameters initialized to 0.
-    Oscillator( void );
-     
+    Oscillator(void);
+
     //  Copy, assignment, and destruction are free.
     //
     //  Copied and assigned Oscillators have the duplicate state
     //  variables and the filters have the same coefficients,
     //  but the state of the filter delay lines is not copied.
 
-// --- oscillation ---
-     
-    //! Reset the instantaneous envelope parameters 
+    // --- oscillation ---
+
+    //! Reset the instantaneous envelope parameters
     //! (frequency, amplitude, bandwidth, and phase).
-    //! The sample rate is needed to convert the 
+    //! The sample rate is needed to convert the
     //! Breakpoint frequency (Hz) to radians per sample.
-    void resetEnvelopes( const Breakpoint & bp, double srate );
-      
+    virtual void resetEnvelopes(const Breakpoint& bp, double srate);
+
     //! Reset the phase of the Oscillator to the specified
-    //! value. This is done when the amplitude of a Partial 
+    //! value. This is done when the amplitude of a Partial
     //! goes to zero, so that onsets are preserved in distilled
     //! and collated Partials.
-    void setPhase( double ph );
-    
-    void setFrequencyScaling( double scaling );
+    void setPhase(double ph);
 
     //! Accumulate bandwidth-enhanced sinusoidal samples modulating the
     //! oscillator state from its current values of radian frequency, amplitude,
@@ -107,36 +105,49 @@ public:
     //! ending before end (no sample is accumulated at end). The caller must
     //! insure that the indices are valid. Target frequency and bandwidth are
     //! checked to prevent aliasing and bogus bandwidth enhancement.
-    void oscillate( double * begin, double * end,
-                    const Breakpoint & bp, double srate);
+    void oscillate(double* begin, double* end,
+                   const Breakpoint& bp, double srate);
 
-// --- accessors ---
+    // --- accessors ---
 
     //! Return the instantaneous amplitde of the Oscillator.
-    double amplitude( void ) const { return m_instamplitude; }
-    
+    double amplitude(void) const
+    {
+        return m_instamplitude;
+    }
+
     //! Return the instantaneous bandwidth of the Oscillator.
-    double bandwidth( void ) const { return m_instbandwidth; }
-    
+    double bandwidth(void) const
+    {
+        return m_instbandwidth;
+    }
+
     //! Return the instantaneous phase of the Oscillator.
-    double phase( void ) const { return m_determphase; }
-    
+    double phase(void) const
+    {
+        return m_determphase;
+    }
+
     //! Return the instantaneous radian frequency of the Oscillator.
-    double radianFreq( void ) const { return m_instfrequency; }
-    
-    double frequencyScalig( void ) const { return m_frequencyScaling; }
-    
-    //! Return access to the Filter used by this oscillator to 
+    double radianFreq(void) const
+    {
+        return m_instfrequency;
+    }
+
+    //! Return access to the Filter used by this oscillator to
     //! implement bandwidth-enhanced sinusoidal synthesis.
-    Filter & filter( void ) { return m_filter; }
-    
-// --- static members ---
+    Filter& filter(void)
+    {
+        return m_filter;
+    }
+
+    // --- static members ---
 
     //! Static local function for obtaining a prototype Filter
     //! to use in Oscillator construction. Eventually, allow
     //! external (client) specification of the Filter prototype.
-    static const Filter & prototype_filter( void );
-     
+    static const Filter& prototype_filter(void);
+
 };  //  end of class Oscillator
 
 }   //  end of namespace Loris
